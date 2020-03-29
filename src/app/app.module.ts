52e6fc4b6bfc89bbd68ff28014e70dbe9bcd03ee
Alejandro Mur <alejandro.mur@boream.com>
2020-03-29 16:44:31 +0200

    Add nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from "./app.component";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { environment } from "../environments/environment";
+import { NavComponent } from './components/shared/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
