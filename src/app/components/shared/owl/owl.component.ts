8baa120b3f2d8628d84811be9dad9dd2eeeb24b3
Alejandro Mur <alejandro.mur@boream.com>
2020-04-12 16:48:54 +0200

    Refactor owl to shared component

--- src/app/components/shared/owl/owl.component.ts
+++ src/app/components/shared/owl/owl.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { FirebaseService } from "src/app/services/firebase.service";
 
 @Component({
-  selector: 'app-owl',
-  templateUrl: './owl.component.html',
-  styleUrls: ['./owl.component.scss']
+  selector: "app-owl",
+  templateUrl: "./owl.component.html",
+  styleUrls: ["./owl.component.scss"],
 })
 export class OwlComponent implements OnInit {
+  @Input() item: any;
 
-  constructor() { }
+  constructor(public firebaseService: FirebaseService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onGetDetails(owl: any) {
+    this.firebaseService.selectedOwl = Object.assign({}, owl);
   }
-
 }
