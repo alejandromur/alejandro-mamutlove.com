89d0525230abafbcc50c5c1083708574c8d8079f
Alejandro Mur <alejandro.mur@boream.com>
2020-04-19 23:19:16 +0200

    Append Search component to List component

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { FirebaseService } from "src/app/services/firebase.service";
-import { IdsService } from "src/app/services/ids.service";
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: "app-list",
@@ -8,12 +7,15 @@ import { IdsService } from "src/app/services/ids.service";
   styleUrls: ["./list.component.scss"],
 })
 export class ListComponent implements OnInit {
-  items: any;
+  noResults = false;
+
+  rawItems: any = [];
+  items: any = [];
+
   dictionary: { key: string; id: number }[];
 
   constructor(
     public firebaseService: FirebaseService,
-    public idsService: IdsService
   ) {}
 
   ngOnInit() {
@@ -25,10 +27,30 @@ export class ListComponent implements OnInit {
       .getList()
       .snapshotChanges()
       .subscribe((result) => {
-        this.items = result.reverse().map((c: any) => ({
+        this.rawItems = result.reverse().map((c: any) => ({
           key: c.payload.key,
           ...c.payload.val(),
         }));
+        this.items = [...this.rawItems];
       });
+  }
+
+  onSearch(search: string[]) {
+    const [channel, query] = [...search];
+
+    this.items = this.rawItems.filter((item) => {
+      if (channel === "id") {
+        let channelId = item.id;
+        channelId = channelId.toString();
+        return channelId.includes(query);
+      }
+      return item[channel].includes(query);
+    });
+
+    if (!this.items.length) {
+      this.noResults = true;
+    } else {
+      this.noResults = false;
+    }
   }
 }
