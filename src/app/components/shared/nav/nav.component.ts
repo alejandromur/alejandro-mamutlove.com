eb1ad0a6983eee97db25e3267415110ced6138c9
Alejandro Mur <alejandro.mur@boream.com>
2020-04-19 23:24:16 +0200

    Subscribe to auth service

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -1,45 +1,58 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { MenuInterface } from "src/app/types/menu-interface";
-
-const MENU: MenuInterface[] = [
-  {
-    icon: "list",
-    text: "Listado",
-    link: "/listado",
-  },
-  {
-    icon: "favs",
-    text: "Favoritos",
-    link: "/favoritos",
-  },
-  {
-    icon: "search",
-    text: "Buscar",
-    link: "/buscar",
-  },
-  {
-    icon: "edit",
-    text: "Nuevo",
-    link: "/nuevo",
-  },
-];
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuInterface } from 'src/app/types/menu-interface';
+import { AuthService } from 'src/app/services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from 'firebase';
 
 @Component({
-  selector: "app-nav",
-  templateUrl: "./nav.component.html",
-  styleUrls: ["./nav.component.scss"],
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  user: Observable<User | null>;
   menu: MenuInterface[];
 
-  constructor(private router: Router) {}
+  PUBLIC_MENU: MenuInterface[] = [
+    {
+      icon: 'list',
+      text: 'Listado',
+      link: '/listado',
+    },
+  ];
+
+  PRIVATE_MENU: MenuInterface[] = [
+    ...this.PUBLIC_MENU,
+    {
+      icon: 'favs',
+      text: 'Favoritos',
+      link: '/favoritos',
+    },
+    {
+      icon: 'edit',
+      text: 'Nuevo',
+      link: '/nuevo',
+    },
+  ];
+
+  constructor(private router: Router, public authService: AuthService) {}
 
   ngOnInit() {
-    this.menu = MENU;
+    this.authService.user.subscribe((user: any) => {
+      // console.log(user);
+      this.user = user;
+      this.menu = this.user ? this.PRIVATE_MENU : this.PUBLIC_MENU;
+    });
   }
 
   addIconFromSprite(icon: string) {
     return `assets/images/sprite.svg#${icon}`;
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.menu = this.PUBLIC_MENU;
+    this.router.navigate(['/login']);
   }
 }
