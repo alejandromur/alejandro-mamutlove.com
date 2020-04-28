fc5964377ef73ab111353bb689ccd327cb026408
Alejandro Mur <alejandro.mur@boream.com>
2020-04-28 23:29:45 +0200

    Update Form component to save and edit book details

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,28 +1,21 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { FirebaseService } from "../../services/firebase.service";
-import { IdsService } from "src/app/services/ids.service";
-import { ActivatedRoute } from "@angular/router";
-import { IdsInterface } from "src/app/models/ids";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FirebaseService } from '../../services/firebase.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: "app-form",
-  templateUrl: "./form.component.html",
-  styleUrls: ["./form.component.scss"],
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  form: FormGroup;
-
   action: string;
-
-  owl: any;
-  key: string;
-  dict: IdsInterface;
+  form: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
-    public firebaseService: FirebaseService,
-    public idsService: IdsService
+    private router: Router,
+    public firebaseService: FirebaseService
   ) {}
 
   ngOnInit() {
@@ -33,67 +26,94 @@ export class FormComponent implements OnInit {
   }
 
   initForm(data: any): void {
-    // console.log(this.firebaseService.getSelectedOwl());
-    // console.log(this.firebaseService.selectedOwl);
-    if (data.action === "edit") {
+    // console.log(this.firebaseService.getSelectedItem());
+    // console.log(this.firebaseService.selectedItem);
+    if (data.action === 'edit') {
       this.form = new FormGroup({
-        key: new FormControl(this.firebaseService.selectedOwl.key),
+        key: new FormControl(this.firebaseService.selectedItem.key),
         id: new FormControl(
-          this.firebaseService.selectedOwl.id,
+          this.firebaseService.selectedItem.id,
           Validators.required
         ),
         image: new FormControl(
-          this.firebaseService.selectedOwl.image
+          this.firebaseService.selectedItem.image
         ),
-        person: new FormControl(
-          this.firebaseService.selectedOwl.person,
+        author: new FormControl(
+          this.firebaseService.selectedItem.author,
           Validators.required
         ),
-        date: new FormControl(
-          this.firebaseService.selectedOwl.date,
+        title: new FormControl(
+          this.firebaseService.selectedItem.title,
           Validators.required
         ),
-        location: new FormControl(
-          this.firebaseService.selectedOwl.location,
+        year: new FormControl(
+          this.firebaseService.selectedItem.year,
+          Validators.required
+        ),
+        rating: new FormControl(
+          this.firebaseService.selectedItem.rating,
+          Validators.required
+        ),
+        favourite: new FormControl(
+          this.firebaseService.selectedItem.favourite,
+          Validators.required
+        ),
+        isbn: new FormControl(
+          this.firebaseService.selectedItem.isbn,
+          Validators.required
+        ),
+        pages: new FormControl(
+          this.firebaseService.selectedItem.pages,
+          Validators.required
+        ),
+        editorial: new FormControl(
+          this.firebaseService.selectedItem.editorial,
           Validators.required
         ),
         category: new FormControl(
-          this.firebaseService.selectedOwl.category,
+          this.firebaseService.selectedItem.category,
           Validators.required
         ),
         comment: new FormControl(
-          this.firebaseService.selectedOwl.comment,
+          this.firebaseService.selectedItem.comment,
           Validators.required
         ),
       });
-    } else if (data.action === "new") {
+    } else if (data.action === 'new') {
       this.form = new FormGroup({
         key: new FormControl(null),
-        id: new FormControl("", Validators.required),
-        image: new FormControl(""),
-        person: new FormControl("", Validators.required),
-        date: new FormControl("", Validators.required),
-        location: new FormControl("", Validators.required),
-        category: new FormControl("", Validators.required),
-        comment: new FormControl("", Validators.required),
+        id: new FormControl('', Validators.required),
+        image: new FormControl(''),
+        author: new FormControl('', Validators.required),
+        title: new FormControl('', Validators.required),
+        year: new FormControl('', Validators.required),
+        rating: new FormControl('', Validators.required),
+        favourite: new FormControl(''),
+        isbn: new FormControl(''),
+        pages: new FormControl(''),
+        editorial: new FormControl(''),
+        category: new FormControl('', Validators.required),
+        comment: new FormControl('', Validators.required),
       });
     }
   }
 
   resetForm(): void {
     this.form.reset();
-    this.initForm({ action: "new" });
+    this.initForm({ action: 'new' });
   }
 
   onSubmit(event: any) {
-    console.warn(this.form.value);
-    if (this.action === "edit") {
+    if (this.action === 'edit') {
       this.firebaseService.edit(this.form.value).then((res) => {
+        this.firebaseService.updateList();
         this.resetForm();
+        this.router.navigate(['/listado']);
       });
-    } else if (this.action === "new") {
+    } else if (this.action === 'new') {
       this.firebaseService.create(this.form.value).then((res) => {
         this.resetForm();
+        this.router.navigate(['/listado']);
       });
     }
   }
